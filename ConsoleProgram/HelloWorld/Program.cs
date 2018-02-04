c7e4d98578c5529daf93d8d2ad59dee97bf3a7c0
CodeEremite <codefighter@foxmail.com>
2018-02-04 20:40:24 +0800

    C#: 泛型使用学习、正则表达式处理文本“

--- ConsoleProgram/HelloWorld/Program.cs
+++ ConsoleProgram/HelloWorld/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace HelloWorld
 {
@@ -6,7 +8,39 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            string text = @"Do you like net core?
+                            This the first example.
+                            Do you know me?
+                            This is LuoYin.";
+
+            Dictionary<string,int> frequencies = CountWords(text);
+
+            foreach(KeyValuePair<string,int> entry in frequencies)
+            {
+                Console.WriteLine("{0}:{1}",entry.Key,entry.Value);
+            }
+
             Console.WriteLine("Hello World!");
+        }
+
+        static Dictionary<string,int> CountWords(string text)
+        {
+            Dictionary<string,int> frequencies = new Dictionary<string,int>();
+
+            string[] words = Regex.Split(text,@"\W+");
+            
+            foreach(string word in words)
+            {
+                if(frequencies.ContainsKey(word))
+                {
+                    frequencies[word]++;
+                }
+                else
+                {
+                    frequencies[word]=1;
+                }
+            }
+            return frequencies;
         }
     }
 }
